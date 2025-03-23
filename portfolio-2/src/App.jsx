1b3d37cfd818bd1b6c15cf81e66eae31e0ea875d
mattp532 <youremail@example.com>
2025-03-23 01:28:15 -0400

    chanes

--- portfolio-2/src/App.jsx
+++ portfolio-2/src/App.jsx
@@ -54,7 +54,7 @@ function App() {
             }}
           >
             <motion.h1 className="fade-down font-bold text-4xl">
-              Hi, I'm Matthew
+            👋 Hi, I'm Matthew
             </motion.h1>
             <h2 className=" text-xl text-gray-300 font-normal mt-3 ">
               CS Student & Software Developer
@@ -154,18 +154,18 @@ function App() {
                   }}
           
           className="mt-10">
-            <h1 className="font-bold">About me</h1>
-            <ul className="list-disc">
+            <h1 className="font-bold text-2xl">About me</h1>
+            <ul className="list-disc space-y-2 mt-2">
               <li className="hover:translate-x-1 duration-300">
-                  1st year CS student @TMU
+              🖥️ 1st year CS student @TMU
               </li>
-              <li className="hover:translate-x-1 duration-300">                Experience in building frontend projects
-              </li>
-              <li className="hover:translate-x-1 duration-300">
-                Learning backend - API development and database management
+              <li className="hover:translate-x-1 duration-300">               🎨 Experience in building frontend projects
               </li>
               <li className="hover:translate-x-1 duration-300">
-                I also like making music and learning Cantonese :-)
+              🌐 Learning backend - API development and database management
+              </li>
+              <li className="hover:translate-x-1 duration-300">
+              🎶 I also like making music and learning Cantonese :-)
               </li>
               
             </ul>
