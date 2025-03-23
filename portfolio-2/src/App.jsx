9a624f4a4d21797a2fa8a91505a02f9337720c37
mattp532 <youremail@example.com>
2025-03-23 13:25:14 -0400

    test

--- portfolio-2/src/App.jsx
+++ portfolio-2/src/App.jsx
@@ -45,13 +45,12 @@ function App() {
             <div className="absolute top-4/100 left-9/100 flex lg: left-12/100 xl:left-24/100">
               <motion.img
                 initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
+                animate={{ opacity: 100 }}
                 transition={{
                   duration: 0.6,
-                  ease: "easeOut",
                 }}
                 key="pfp"
-                className="hover:scale-105 duration-300 z-10 h-30 w-30 sm:h-32 sm:w-32 md:h-33 md:w-33 lg:w-40 lg:h-40 xl:h-35 xl:w-35 rounded-full border-2 border-gray-500"
+                className="xl:hover:scale-105 xl:duration-300 z-10 h-30 w-30 sm:h-32 sm:w-32 md:h-33 md:w-33 lg:w-40 lg:h-40 xl:h-35 xl:w-35 rounded-full border-2 border-gray-500"
                 src={pfp}
                 alt=""
               />
